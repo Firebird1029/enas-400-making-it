ac11d7288a5c0b6719d86c95f46d98caec9c0a2d
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2023-11-30 23:18:55 -0500

    working 5-button sine output

---
+++ meowzart_v1/meowzart_v1.ino
@@ -0,0 +1,143 @@
+#include <Audio.h>
+#include <Bounce2.h>
+#include <SD.h>
+#include <SPI.h>
+#include <SerialFlash.h>
+#include <Wire.h>
+
+/*
+ * FUTURE
+ *
+ * new timbres, button to change timbre
+ * volume knob
+ * sounds from SD card (e.g. bird call)
+ */
+
+#define MASTER_VOLUME 0.5  // <= 0.8 to prevent distortion
+
+#define BASE_FREQ 349.23  // F4, default tonic
+
+// MUSIC
+#define WAVEFORM_VOLUME 0.3
+#define ENV_ATTACK 10.5
+#define ENV_HOLD 30    // 2.5
+#define ENV_DECAY 400  // 35
+#define ENV_SUSTAIN 0  // 0.8
+#define ENV_RELEASE 300
+
+enum SCALE_TYPE { MAJOR, MINOR, PENTATONIC };
+
+// PINOUT
+#define BTN_1_PIN 28
+#define BTN_2_PIN 29
+#define BTN_3_PIN 30
+#define BTN_4_PIN 31
+#define BTN_5_PIN 32
+
+// BUTTONS
+Button btn1 = Button();
+Button btn2 = Button();
+Button btn3 = Button();
+Button btn4 = Button();
+Button btn5 = Button();
+
+#define NUM_BUTTONS 5
+Button allButtons[] = {btn1, btn2, btn3, btn4, btn5};
+const int BTN_PINS[] = {BTN_1_PIN, BTN_2_PIN, BTN_3_PIN, BTN_4_PIN, BTN_5_PIN};
+
+// GLOBAL VARIABLES
+// elapsedMillis timer;  // master timer (auto-incrementing)
+
+// PJRC CODE
+
+// GUItool: begin automatically generated code
+AudioSynthWaveform waveform1;   // xy=107,85
+AudioEffectEnvelope envelope1;  // xy=282,85
+AudioOutputI2S i2s1;            // xy=457,85
+AudioConnection patchCord1(waveform1, envelope1);
+AudioConnection patchCord2(envelope1, 0, i2s1, 0);
+AudioConnection patchCord3(envelope1, 0, i2s1, 1);
+AudioControlSGTL5000 sgtl5000_1;  // xy=64.5,20
+// GUItool: end automatically generated code
+
+void setup(void) {
+  // SERIAL SETUP
+  Serial.begin(115200);
+  Serial.println("Serial started");
+
+  // INIT AUDIO SETUP
+  AudioMemory(200);
+  AudioNoInterrupts();
+
+  // SYNTH SETUP
+  waveform1.begin(WAVEFORM_VOLUME, BASE_FREQ, WAVEFORM_SINE);
+  envelope1.attack(ENV_ATTACK);
+  envelope1.hold(ENV_HOLD);
+  envelope1.decay(ENV_DECAY);
+  envelope1.sustain(ENV_SUSTAIN);
+  envelope1.release(ENV_RELEASE);
+
+  // FINISH AUDIO SETUP
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(MASTER_VOLUME);
+  AudioInterrupts();
+
+  // BUTTON SETUP
+  btn1.attach(BTN_1_PIN, INPUT_PULLUP);
+  btn1.interval(5);
+  btn1.setPressedState(LOW);
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    allButtons[i].attach(BTN_PINS[i], INPUT_PULLUP);
+    allButtons[i].interval(5);
+    allButtons[i].setPressedState(LOW);
+  }
+
+  // SETUP DONE
+  Serial.println("Finished setup.");
+  delay(100);
+}
+
+void loop() {
+  // update buttons
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    allButtons[i].update();
+  }
+
+  // if a button is pressed, play a note
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    if (allButtons[i].fell()) {
+      Serial.print("Button ");
+      Serial.print(i);
+      Serial.println(" pressed");
+
+      int semitones = mapScale(PENTATONIC, i);
+      waveform1.frequency(BASE_FREQ * pow(2, semitones / 12.0));
+
+      envelope1.noteOn();
+    }
+  }
+
+  delay(10);
+}
+
+// HELPER FUNCTIONS
+const int majorScale[8] = {0, 2, 4, 5, 7, 9, 11, 12};
+const int minorScale[8] = {0, 2, 3, 5, 7, 8, 10, 12};
+const int pentatonicScale[6] = {0, 2, 4, 7, 9, 12};
+int mapScale(SCALE_TYPE scaleType, int step) {
+  if (step == -1) {
+    return -1;
+  }
+  if (scaleType == MAJOR) {
+    // convert step (from 0-6 inclusive) to a major scale
+    return majorScale[step];
+  } else if (scaleType == MINOR) {
+    // convert step (from 0-6 inclusive) to a minor scale
+    return minorScale[step];
+  } else if (scaleType == PENTATONIC) {
+    // convert step (from 0-5 inclusive) to a pentatonic scale
+    return pentatonicScale[step];
+  }
+
+  return step;
+}
